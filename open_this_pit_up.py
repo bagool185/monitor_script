1b0f31927256be0018fc457c3b67116308fddac9
bagool185 <ioan.adrian98@gmail.com>
2017-07-04 15:31:41 +0300

    added comments

--- open_this_pit_up.py
+++ open_this_pit_up.py
@@ -2,15 +2,25 @@ import time
 import requests
 import webbrowser
 
+#replace the url with whatever you need to monitorize
 url = 'http://www.example.com'
 
 action_item = requests.get(url)
 
+#get the length of the content
 current_length = len(action_item.content)
+
+# get the length of the initial content, which is stored in compare.txt
+# if you want, you can get rid of the file thing and hardcode the length 
+# e.g.
+#if current_length <> 10000:
+#	....
 
 file = open("compare.txt", "r")
 old_length = int(file.read())
 
+# if the length of the 2 contents differs, then the site was updated
+# and your default browser will open at the specified url
 if old_length <> current_length:
 	webbrowser.open_new(url)
 else:
