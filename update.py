150e59077361a734c5c0637c9a3687f4a4f55bd9
bagool185 <ioan.adrian98@gmail.com>
2017-07-04 21:39:04 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,11 +1,14 @@
+from bs4 import BeautifulSoup
 import requests
 
 url = 'http://www.example.com'
 
 action_item = requests.get(url)
 
-length = len(action_item.content)
+soup = BeautifulSoup(action_item.content, "html5lib")
+
+hyperlinks = soup.find_all("a")
 
 file = open("compare.txt", "w+")
 
-file.write(str(length))
+file.write(str(hyperlinks))
